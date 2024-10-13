0146bb4221c2c9fd1fc00747836e43a689e6227b
ashiroot777 <ashritha.sankara@gmail.com>
2024-10-13 09:43:34 +0530

    functions

---
+++ project_files/func_module.py
@@ -0,0 +1,34 @@
+#functionmodule
+
+import random 
+
+#creating function for generating salt
+
+def generate_salt():
+# making a list of anions and cations
+    anions=["Carbonate","Chloride","Bromide","Iodide","Nitrate","Sulphate"]
+    cations=["Ammonium","Aluminium","Zinc","Strontium","Magnesium","Manganese","Calcium","Barium","Cadmium"]
+
+#using random to generate them
+    a=random.choice(anions)
+    c=random.choice(cations)
+
+#concatenating both the strings to form the variable which stores salt name
+    salt = c + '' + a 
+
+    return salt
+
+#creating function for establishing connection
+
+def connect_mysql(pass_word):
+#taking password from user
+    password = str(input('Enter your MySQL password : '))
+    import mysql.connector as m
+#connecting
+    global mydb
+    mydb=m.connect(host='localhost',user='root',password=pass_word)
+#creating cursor
+    global c
+    c=mydb.cursor()
+
+    print('connection established!')
